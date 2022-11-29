80c8a22579e11a556932594c75fb86d200776da3
AliyevVilayat <77es4pa@code.edu.az>
2022-11-29 12:47:13 +0400

    added 11/29/2022

---
+++ Practica8/Practica8/Method.cs
@@ -0,0 +1,24 @@
+﻿namespace Practica8;
+
+public class Method<T>
+{
+
+    public int FindElement<T>(T[] arr, DelegateMethod<T> method)
+    {
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (method(arr[i]))
+            {
+                return i;
+
+            }
+        }      
+
+        return -1;
+    }
+
+    public delegate bool DelegateMethod<T>(T value);
+
+   
+}
---
+++ Practica8/Practica8/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Practica8;
+
+Method<int> method = new();
+
+Console.WriteLine(method.FindElement(new int[] { -1, 2, 3, 4 }, Positive));
+
+bool Positive(int value)
+{
+
+    if (value > 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool Negative(int value)
+{
+
+    return false;
+}
+
+
